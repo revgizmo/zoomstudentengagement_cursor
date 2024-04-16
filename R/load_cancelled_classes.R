6782af8f7c4d5e839dbb51b361366c55f688ddc8
Conor Healy <revgizmo@gmail.com>
2024-04-15 21:27:04 -0700

    typo cleanup

--- R/load_cancelled_classes.R
+++ R/load_cancelled_classes.R
@@ -2,7 +2,6 @@
 #'
 #' This function creates a tibble from a provided csv file of cancelled class
 #' sessions for scheduled classes where a zoom recording is not expected.
-#' transcript files of Zoom recordings.
 #'
 #' @param data_folder Overall data folder for your recordings and data. Defaults
 #'   to 'data'
@@ -21,11 +20,19 @@ load_cancelled_classes <-
   function(data_folder = "data",
            cancelled_classes_file = "cancelled_classes.csv",
            cancelled_classes_col_types = "ccccccccnnnncTTcTTccci") {
-    cancelled_classes_file_path <- paste0(data_folder, "/", cancelled_classes_file)
+    cancelled_classes_file_path <-
+      paste0(data_folder, "/", cancelled_classes_file)
 
+    # Check if the file exists
     if (file.exists(cancelled_classes_file_path)) {
-      readr::read_csv(cancelled_classes_file_path,
-        col_types = cancelled_classes_col_types
-      )
+      # File exists, proceed with importing it
+      data <- readr::read_csv(cancelled_classes_file_path,
+                              col_types = cancelled_classes_col_types)
+    } else {
+      # File doesn't exist, handle the situation accordingly
+      print(paste("File does not exist:", cancelled_classes_file_path))
+      data <- zoomstudentengagement::make_blank_cancelled_classes_df()
     }
+
+    data
   }
