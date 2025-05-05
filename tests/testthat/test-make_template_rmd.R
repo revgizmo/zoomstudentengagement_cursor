b9485501147ad99f6b89ca56233913e33c884fa1
Conor Healy <revgizmo@gmail.com>
2025-05-04 21:20:33 -0700

    Fix make_template_rmd to return TRUE/FALSE and only print on success; add robust tests

---
+++ tests/testthat/test-make_template_rmd.R
@@ -0,0 +1,27 @@
+test_that("make_template_rmd copies the template file and prints a message", {
+  temp_dir <- tempdir()
+  old_wd <- setwd(temp_dir)
+  on.exit(setwd(old_wd))
+  # Create a dummy template file
+  template_file <- file.path(temp_dir, "dummy_template.Rmd")
+  writeLines(c("# Dummy Template", "Some content."), template_file)
+  new_template_file <- file.path(temp_dir, "copied_template.Rmd")
+  # Should copy the file and print a message
+  expect_output(make_template_rmd(new_template_file_name = new_template_file, template_file = template_file), "created")
+  expect_true(file.exists(new_template_file))
+  expect_equal(readLines(new_template_file), readLines(template_file))
+  unlink(template_file)
+  unlink(new_template_file)
+})
+
+test_that("make_template_rmd handles missing template file gracefully", {
+  temp_dir <- tempdir()
+  old_wd <- setwd(temp_dir)
+  on.exit(setwd(old_wd))
+  missing_file <- file.path(temp_dir, "does_not_exist.Rmd")
+  new_template_file <- file.path(temp_dir, "should_not_exist.Rmd")
+  expect_false(file.exists(missing_file))
+  expect_false(file.exists(new_template_file))
+  expect_false(make_template_rmd(new_template_file_name = new_template_file, template_file = missing_file))
+  expect_false(file.exists(new_template_file))
+}) 
