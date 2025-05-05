4e4935153c74f18a8dd84feaaf4addb63075eba3
Conor Healy <revgizmo@gmail.com>
2025-05-05 08:32:54 -0700

    Fix col_types in make_blank_cancelled_classes_df for correct integer/character columns; revert test to match

--- R/make_blank_cancelled_classes_df.R
+++ R/make_blank_cancelled_classes_df.R
@@ -13,7 +13,7 @@
 make_blank_cancelled_classes_df <- function() {
   readr::read_csv(
     I("dept,section,day,time,instructor,Topic,ID,Start Time,File Size (MB),File Count,Total Views,Total Downloads,Last Accessed,match_start_time,match_end_time,dt,recording_start,start_time_local,transcript_file,chat_file,closed_caption_file"),
-    col_types = "ccccccccdnnnncTTcTTccci"
+    col_types = "ccccccccdiiicTTcTTccci"
   ) %>%
     dplyr::mutate(
       match_start_time = as.POSIXct(match_start_time, tz = "America/Los_Angeles"),
