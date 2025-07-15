68a8e92e2e4a4a2f88f7ef1c3c96f2c0e30f7e40
Conor Healy <revgizmo@gmail.com>
2025-07-14 19:58:44 -0700

    Fix make_template_rmd function: rename to make_new_analysis_template and fix template path
    - Rename function from make_template_rmd to make_new_analysis_template
    - Copy README.Rmd to inst/new_analysis_template.Rmd as the template file
    - Fix documentation to accurately describe the function's purpose
    - Update template path to use system.file() correctly
    - Update all tests and examples to use new function name
    - Update NAMESPACE to export the new function
    - All tests passing

--- tests/testthat/test-make_new_analysis_template.R
+++ tests/testthat/test-make_new_analysis_template.R
@@ -1,4 +1,4 @@
-test_that("make_template_rmd copies the template file and prints a message", {
+test_that("make_new_analysis_template copies the template file and prints a message", {
   temp_dir <- tempdir()
   old_wd <- setwd(temp_dir)
   on.exit(setwd(old_wd))
@@ -7,14 +7,14 @@ test_that("make_template_rmd copies the template file and prints a message", {
   writeLines(c("# Dummy Template", "Some content."), template_file)
   new_template_file <- file.path(temp_dir, "copied_template.Rmd")
   # Should copy the file and print a message
-  expect_output(make_template_rmd(new_template_file_name = new_template_file, template_file = template_file), "created")
+  expect_output(make_new_analysis_template(new_template_file_name = new_template_file, template_file = template_file), "created")
   expect_true(file.exists(new_template_file))
   expect_equal(readLines(new_template_file), readLines(template_file))
   unlink(template_file)
   unlink(new_template_file)
 })
 
-test_that("make_template_rmd handles missing template file gracefully", {
+test_that("make_new_analysis_template handles missing template file gracefully", {
   temp_dir <- tempdir()
   old_wd <- setwd(temp_dir)
   on.exit(setwd(old_wd))
@@ -22,6 +22,6 @@ test_that("make_template_rmd handles missing template file gracefully", {
   new_template_file <- file.path(temp_dir, "should_not_exist.Rmd")
   expect_false(file.exists(missing_file))
   expect_false(file.exists(new_template_file))
-  expect_false(make_template_rmd(new_template_file_name = new_template_file, template_file = missing_file))
+  expect_false(make_new_analysis_template(new_template_file_name = new_template_file, template_file = missing_file))
   expect_false(file.exists(new_template_file))
 })
